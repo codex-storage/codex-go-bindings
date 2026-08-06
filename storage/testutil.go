168909868940e2c44e510a730ec96bc54cf7de97
Arnaud <arnaud@status.im>
2026-08-06 12:14:09 +0400

    Simplify test config

--- storage/testutil.go
+++ storage/testutil.go
@@ -22,58 +22,23 @@ func newStorageNode(t *testing.T, opts ...Config) *StorageNode {
 	config := defaultConfigHelper(t)
 
 	if len(opts) > 0 {
-		c := opts[0]
+		defaults := config
+		config = opts[0]
 
-		if c.BlockRetries > 0 {
-			config.BlockRetries = c.BlockRetries
+		if config.DataDir == "" {
+			config.DataDir = defaults.DataDir
 		}
 
-		if c.LogLevel != "" {
-			config.LogLevel = c.LogLevel
+		if config.LogFormat == "" {
+			config.LogFormat = defaults.LogFormat
 		}
 
-		if c.LogFile != "" {
-			config.LogFile = c.LogFile
+		if config.BlockRetries == 0 {
+			config.BlockRetries = defaults.BlockRetries
 		}
 
-		if len(c.BootstrapNodes) != 0 {
-			config.BootstrapNodes = c.BootstrapNodes
-		}
-
-		if c.NoBootstrapNode {
-			config.NoBootstrapNode = true
-		}
-
-		if c.MixEnabled {
-			config.MixEnabled = true
-		}
-
-		if len(c.DhtMixProxies) != 0 {
-			config.DhtMixProxies = c.DhtMixProxies
-		}
-
-		if c.MixPoolJson != "" {
-			config.MixPoolJson = c.MixPoolJson
-		}
-
-		if c.DiscoveryPort != 0 {
-			config.DiscoveryPort = c.DiscoveryPort
-		}
-
-		if c.StorageQuota != 0 {
-			config.StorageQuota = c.StorageQuota
-		}
-
-		if c.NumThreads != 0 {
-			config.NumThreads = c.NumThreads
-		}
-
-		if c.BlockTtl != "" {
-			config.BlockTtl = c.BlockTtl
-		}
-
-		if c.BlockMaintenanceInterval != "" {
-			config.BlockMaintenanceInterval = c.BlockMaintenanceInterval
+		if config.Nat == "" {
+			config.Nat = defaults.Nat
 		}
 	}
 
